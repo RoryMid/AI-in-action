61c173d605445a1dfc775eae8180362e0d166f94
RoryMid <rorymiddleton@yahoo.co.uk>
2025-05-16 13:17:05 +0100

    update typos

--- tests/test_ingestion.py
+++ tests/test_ingestion.py
@@ -1,10 +1,4 @@
 def test_mongo_insert(monkeypatch):
-    import ingestion.oop.mongo_connector as mongo_connector
-
-    monkeypatch.setattr(mongo_connector, "MongoClient", lambda uri: MockClient())
-    mongo = mongo_connector.MongoConnector()
-    mongo.insert_document("test.pdf", "content here")
-
     class MockCollection:
         def insert_one(self, doc):
             assert "filename" in doc
@@ -14,6 +8,8 @@ def test_mongo_insert(monkeypatch):
         def __getitem__(self, name):
             return {"documents": MockCollection()}
 
-    monkeypatch.setattr("pymongo.MongoClient", lambda uri: MockClient())
-    mongo = MongoConnector()
+    import ingestion.oop.mongo_connector as mongo_connector
+
+    monkeypatch.setattr(mongo_connector, "MongoClient", lambda uri: MockClient())
+    mongo = mongo_connector.MongoConnector()
     mongo.insert_document("test.pdf", "content here")
